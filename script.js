7958088d3e829510d201eb7d363a5638b957cf76
Reka Nagy <Reka.Nagy@rsmuk.com>
2020-12-16 11:39:15 +0000

    ToDos

---
+++ script.js
@@ -0,0 +1,8 @@
+// TODO
+// 1. Display intro
+// 2. define array for valid vowels
+// 3. Add  initial song as array of characters
+// 4. Ask user for input
+// 5. Validate input - if valid vowel - continue . Else display alert and return
+// 6. Find all the vowels in the initial song array and replace them all with the input vowel.
+// 7. Display the even sillier song
